2f4bf1abf2d859e265fc8d81931ffdb676bada85
user <amin7061205@gmail.com>
2022-10-11 19:37:45 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int x = sc.nextInt();
+        int a = 2;
+        int b = 1;
+        while ( b <= x) {
+
+            System.out.println(b);
+            b= b * 2;
+
+        }
+    }
+}
+
+
+
